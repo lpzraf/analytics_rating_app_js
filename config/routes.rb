4e7ae8e732e2bdf7c942aa8c6514c5772e061a2c
lpzraf <lpz.raf@gmail.com>
2019-04-24 21:55:31 -0400

    It will add resources to the routes including nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  get 'courses/new'
-  get 'courses/create'
-  get 'courses/show'
-  get 'courses/destroy'
-  get 'categories/new'
-  get 'categories/create'
-  get 'students/new'
-  get 'students/create'
-  get 'students/show'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  # get 'courses/new'
+  # get 'courses/create'
+  # get 'courses/show'
+  # get 'courses/destroy'
+  # get 'categories/new'
+  # get 'categories/create'
+  # get 'students/new'
+  # get 'students/create'
+  # get 'students/show'
+   get 'sessions/new'
+   get 'sessions/create'
+   get 'sessions/destroy'
+
+resources :students
+
+resources :courses do
+  resources :categories
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
