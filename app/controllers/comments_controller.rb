5bc7269ab730811cc2bb5fdece75eaa178ada9f4
lpzraf <lpz.raf@gmail.com>
2019-04-27 16:57:11 -0400

    It will add destroy logic for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
    redirect_to course_path(@course)
  end
 
+ def destroy
+   @course = Course.find(params[:course_id])
+   @comment = @course.comments.find(params[:id])
+   @comment.destroy
+   redirect_to course_path(@course)
+ end
+
  private
    def comment_params
      params.require(:comment).permit(:commenter, :body)
