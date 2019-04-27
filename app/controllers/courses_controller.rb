1a1ca52adb9a374208165ff2484816d1d62b563a
lpzraf <lpz.raf@gmail.com>
2019-04-27 00:21:52 -0400

    It will add course show view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,40 @@
 class CoursesController < ApplicationController
-  def new
-  end
 
-  def create
+  def index
+    @courses = Course.all
   end
 
   def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    @course.save
+    redirect_to @course
+  end
+
+  def update
   end
 
   def destroy
   end
+
+  private
+
+  def course_params
+    params.require(:course).permit(
+      :name,
+      :instructor,
+      #:cat_name????
+    )
+end
 end
