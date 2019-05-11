dee26d806adeacbe5635edaf10dc591321715ed1
lpzraf <lpz.raf@gmail.com>
2019-05-10 22:03:03 -0400

    It will start adding oauth for Facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,64 @@ class SessionsController < ApplicationController
     #@session = Session.new
   end
 
-  def create
-    student = Student.find_by(email: params[:session][:email].downcase)
-      if student && student.authenticate(params[:session][:password])
-        log_in student
-        params[:session][:remember_me] == '1' ? remember(student) : forget(student)
-        #redirect_back_or student
-        redirect_to student
-      else
-        flash.now[:danger] = 'Invalid email/password combination'
-        render 'new'
-      end
-  end
+  # def create
+  #   student = Student.find_by(email: params[:session][:email].downcase)
+  #     if student && student.authenticate(params[:session][:password])
+  #       log_in student
+  #       params[:session][:remember_me] == '1' ? remember(student) : forget(student)
+  #       #redirect_back_or student
+  #       redirect_to student
+  #     else
+  #       flash.now[:danger] = 'Invalid email/password combination'
+  #       render 'new'
+  #     end
+  # end
+###
 
+def create
+    if auth
+        @student = Student.find_by(uid: auth["uid"])
+        if @student
+            session[:student_id] = @student.id
+            redirect_to student_path(@student)
+        else
+            @student = Student.new_student_from_auth(auth)
+            if @student.save
+                session[:student_id] = @student.id
+                redirect_to student_path(@student)
+            else
+                raise @student.errors.full_messages.inspect
+            end
+        end
+    else
+        student = Student.find_by(email: params[:session][:email].downcase)
+          if student && student.authenticate(params[:session][:password])
+            log_in student
+            params[:session][:remember_me] == '1' ? remember(student) : forget(student)
+            #redirect_back_or student
+            redirect_to student
+          else
+            flash.now[:danger] = 'Invalid email/password combination'
+            render 'new'
+        end
+    end
+end
+
+
+
+
+
+
+###
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  #get '/auth/facebook/callback' => 'sessions#create'
+
 
 
   #get 'sessions/login'
