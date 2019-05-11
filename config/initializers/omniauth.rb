0e739fa05e7b86f527997ecf69a1b5365c349070
lpzraf <lpz.raf@gmail.com>
2019-05-10 22:06:27 -0400

    It will eliminate security issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['1285568398265659'], ENV['434e02d096414d87294812eab1bd9087']
+  provider :facebook, ENV[''], ENV['']
 
 end
