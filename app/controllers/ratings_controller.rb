66b0e8310aecf518b5ba3df8e6a25269babf861f
lpzraf <lpz.raf@gmail.com>
2019-05-19 14:15:04 -0400

    It will add redirect to incorrect param ids

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,14 @@
 class RatingsController < ApplicationController
 
+  def index
+    @course = Course.find_by_id(params[:course_id])
+    if @course
+      @ratings = @course.ratings
+    else
+      redirect_to courses_path
+    end 
+  end
+
   def new
 
   end
@@ -7,8 +16,5 @@ class RatingsController < ApplicationController
   def create
   end
 
-  def index
-
-  end
   #look at nested resources lesson.....rating will not save if it doesnt have both course and student id (one comes from curent user method adn the other from the id in the url)
 end
